860e3b7bb2bba5a7290372e7c2c4e33eeb80966e
siyeol97 <leesi2830@naver.com>
2023-05-08 00:48:58 +0900

    05/07

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
-  let post = '강남 우동 맛집';
+  let [title, transTitle] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
+  let [like, transLike] = useState(0);
+  
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4 style={{color : 'red'}}>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{ title[0] } <span onClick={ ()=>{ transLike(like+1)} }>👍</span> { like } <span onClick={ ()=>{ transTitle(['여자 코트 추천', '강남 우동맛집', '파이썬 독학']) } }>성별변경</span> </h4> 
+        <p>5월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[1] }</h4>
+        <p>5월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ title[2] }</h4>
+        <p>5월 17일 발행</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
